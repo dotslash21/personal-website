2e22b1ac6616c7ede9833e5c4f0ed721c57a8cf5
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-05-31 22:33:21 +0530

    feat: Added Tina for content management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
+import Tina from '../.tina/components/TinaDynamicProvider.js'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Tina>
+      <Component {...pageProps} />
+    </Tina>
+  )
 }
 
 export default MyApp
