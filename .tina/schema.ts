03006ba92257a0a7fa7802d0021b183705edcc45
Arunangshu Biswas <arunangshubsws@gmail.com>
2022-06-01 09:10:33 +0530

    feat: Configured Husky

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -1,49 +1,48 @@
-
-import { defineSchema, defineConfig } from "tinacms";
+import { defineSchema, defineConfig } from 'tinacms'
 
 const schema = defineSchema({
   collections: [
     {
-      label: "Blog Posts",
-      name: "posts",
-      path: "content/posts",
+      label: 'Blog Posts',
+      name: 'posts',
+      path: 'content/posts',
       fields: [
         {
-          type: "string",
-          label: "Title",
-          name: "title",
+          type: 'string',
+          label: 'Title',
+          name: 'title',
         },
         {
-          type: "rich-text",
-          label: "Blog Post Body",
-          name: "body",
+          type: 'rich-text',
+          label: 'Blog Post Body',
+          name: 'body',
           isBody: true,
           templates: [
             {
-              name: "PageSection",
-              label: "Page Section",
+              name: 'PageSection',
+              label: 'Page Section',
               fields: [
                 {
-                  type: "string",
-                  name: "heading",
-                  label: "Heading",
+                  type: 'string',
+                  name: 'heading',
+                  label: 'Heading',
                 },
                 {
-                  type: "string",
-                  name: "content",
-                  label: "Content",
+                  type: 'string',
+                  name: 'content',
+                  label: 'Content',
                   ui: {
-                    component: "textarea"
-                  }
-                }
+                    component: 'textarea',
+                  },
+                },
               ],
             },
-          ]
+          ],
         },
       ],
     },
   ],
-});
+})
 
 export default schema
 
@@ -67,19 +66,19 @@ export const tinaConfig = defineConfig({
     /**
      * 1. Import `tinacms` and `RouteMappingPlugin`
      **/
-    import("tinacms").then(({ RouteMappingPlugin }) => {
+    import('tinacms').then(({ RouteMappingPlugin }) => {
       /**
        * 2. Define the `RouteMappingPlugin` see https://tina.io/docs/tinacms-context/#the-routemappingplugin for more details
        **/
       const RouteMapping = new RouteMappingPlugin((collection, document) => {
-        return undefined;
-      });
+        return undefined
+      })
       /**
        * 3. Add the `RouteMappingPlugin` to the `cms`.
        **/
-      cms.plugins.add(RouteMapping);
-    });
+      cms.plugins.add(RouteMapping)
+    })
 
-    return cms;
+    return cms
   },
-});
+})
